b61102e91886bdb66c76aed348b766ab8fa1e90a
sadra zadeh khameneh <sadra.z.kh@gmail.com>
2022-01-22 18:48:33 +0330

    GET & POST

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# POST -> Used to receive data
+# GET -> Used to send data back only
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    return request_data['name']
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    return jsonify({'names': name})
 
 
 if __name__ == '__main__':
